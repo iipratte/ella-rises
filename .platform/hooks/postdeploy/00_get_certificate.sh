8913e4069f456400fecedba14de564cf9240e4a8
iipratte <iipratte@byu.edu>
2025-12-04 19:02:59 -0700

    added certificate lines

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,1 +1,4 @@
 sudo certbot -n -d ella-rises.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email isaac.pratte@gmail.com
+sudo certbot -n -d ella-rises.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email iipratte@byu.edu
+sudo certbot -n -d ella-levantando.is404.net --nginx --agree-tos --email isaac.pratte@gmail.com
+sudo certbot -n -d ella-levantando.is404.net --nginx --agree-tos --email iipratte@byu.edu
