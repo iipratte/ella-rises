ddf2b8b70fdb06e6c9343a44472421e59b643fde
iipratte <iipratte@byu.edu>
2025-12-04 19:12:23 -0700

    moved https domain from eb to cname

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # .platform/hooks/postdeploy/00_get_certificate.sh
-sudo certbot -n -d ella-rises.us-east-2.elasticbeanstalk.com --nginx --agree-tos --email isaac.pratte@gmail.com
+sudo certbot -n -d ella-levantando.is404.net --nginx --agree-tos --email isaac.pratte@gmail.com
